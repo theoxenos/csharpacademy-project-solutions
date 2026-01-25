d7302e8b5d2f85bdbabb16814300445aa54f8f09
Theoxenos <theoxenos@pm.me>
2026-01-25 11:11:02 +0100

    Full project clean up and reformat.

--- Console/SportsResultsNotifier/Utils/MailServerSettings.cs
+++ Console/SportsResultsNotifier/Utils/MailServerSettings.cs
@@ -2,12 +2,12 @@ namespace SportsResultsNotifier.Utils;
 
 public class MailServerSettings
 {
-    public string From { get; set; }
-    public string To { get; set; }
-    public string Subject { get; set; }
-    public string Host { get; set; }
+    public string From { get; set; } = string.Empty;
+    public string To { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Host { get; set; } = string.Empty;
     public int Port { get; set; }
-    public bool SSL { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public bool Ssl { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
